72c92c6226a67a245c558e911a7964ff37f4e1bb
Annia Forney <anniaforney@gmail.com>
2019-03-02 20:21:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function whisper(string) {
+return string.tolowercase()
+}
+console.log(shout) {
+return string.toUpperCase()
+}
+console.log(whisper) {
+  return string.tolowercase()
+}
+sayHiTo ('Grandma')
+sayHiToGrandma(string) {
+  console.log('Hi, Grandma')
+return string. "YES INDEED"
+}
